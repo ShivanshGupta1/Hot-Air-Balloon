7adc32b317346374addf0c79c82c528c4cf69634
Shivansh Gupta <mail@shivanshgupta.com>
2021-05-24 15:00:21 +0530

    Project Done

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var balloon,balloonImage1,balloonImage2, db, result, position;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  db = firebase.database();
+    result = db.ref('balloon/position')
+    result.on('value',(a)=>{
+        console.log(a.val());
+        position = a.val();
+        balloon.x = position.x;
+        balloon.y = position.y;
+    })
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    writePosition(-1,0);
+    //write code to move air balloon in left direction
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    writePosition(1,0);
+    //write code to move air balloon in right direction
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    writePosition(0,-1);
+    //write code to move air balloon in up direction
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    writePosition(0,1);
+    //write code to move air balloon in down direction
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function writePosition(x,y){
+  result.set({
+      'x':balloon.x + x,
+      'y':balloon.y + y
+  })
+}
